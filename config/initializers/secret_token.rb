881145de3b42aa372493d1935f0523e15bfb3134
Erin Leary <ekl9@cornell.edu>
2013-01-09 09:26:57 -0500

    git commit -m "Add all the things"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dhwi::Application.config.secret_token = '7c2be9abaae41da1593b706cd94a18f83fd3583edd96c4727886dd75caf0bd3a86b1d1bf85e6bca5736e788a4df468085bfd5e39530c4a409a30e831424efd68'
