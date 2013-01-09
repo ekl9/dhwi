a3fa4dc71772d0a21ec6005c52d4b6eb44543bee
Erin Leary <ekl9@cornell.edu>
2013-01-09 11:21:22 -0500

    Added votes resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130109145041) do
+ActiveRecord::Schema.define(:version => 20130109161945) do
 
   create_table "topics", :force => true do |t|
     t.string   "title"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(:version => 20130109145041) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "votes", :force => true do |t|
+    t.integer  "topic_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
